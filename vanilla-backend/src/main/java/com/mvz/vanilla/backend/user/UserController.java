e8cbf135427c747c7fbcfba1c45eb0e49fcfbae6
marcvettiger <marc.vettiger@hotmail.com>
2020-01-24 20:44:07 +0100

    initial commit

---
+++ vanilla-backend/src/main/java/com/mvz/vanilla/backend/user/UserController.java
@@ -0,0 +1,13 @@
+package com.mvz.vanilla.backend.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping()
+    public String getUsers(){
+        return "Hi Vanilla";
+    }
+}
